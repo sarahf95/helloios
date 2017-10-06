cec7478d1b035082d5ef0b9b641161eb34874a40
sarahfeldmann <sarahf95@gmail.com>
2017-10-05 17:25:26 -0700

    make code visible

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello-iOS
+//
+//  Created by Sarah Feldmann on 10/5/17.
+//  Copyright © 2017 Sarah Feldmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
